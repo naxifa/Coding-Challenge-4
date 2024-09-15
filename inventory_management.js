69fd610d9e898671e76366ee0572871952e3b455
naxifa <maliha1@usf.edu>
2024-09-14 20:42:46 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,18 @@
+
+// Task 1 - Creating an Inventory Array of Product Objects 
+
+const inventory = [
+{ name: "Laptop", price: 500, quantity: 47, lowStockLevel: 5 },
+{ name: "Smartphone", price: 400, quantity: 96, lowStockLevel: 8 },
+{ name: "Tablet", price: 350, quantity: 56, lowStockLevel: 5 },
+{ name: "Headphones", price: 150, quantity: 35, lowStockLevel: 3 },
+{ name: "Stylus", price: 50, quantity: 25, lowStockLevel: 2 }
+];
+
+console.log(inventory); // An array of products in inventory
+
+
+
+
+
+
